8fb7caf075ce08ce338305058705282f3706576c
Ana Lemos <carol1692@hotmail.com>
2023-11-20 00:47:40 -0300

    commit_inicial

---
+++ static/scripts_js/script.js
@@ -0,0 +1,26 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+const btnSubmit = document.querySelector('.btn');
+// document.addEventListener("DOMContentLoaded", function() {
+//     wrapper.classList.remove('active');
+// });
+
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+})
+
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+})
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+})
+
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+})
+
